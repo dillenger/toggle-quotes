2dadd26d9333e7148c14bc25e4634a8d179d30a2
John Bieling <john.bieling@gmx.de>
2025-04-06 14:15:53 +0200

    Replace Experiment by content script

--- background.js
+++ background.js
@@ -1,9 +1,43 @@
+async function getQuotesCollapsed(tabId) {
+  return browser.storage.session
+    .get({ [`quotes_collapsed_in_tab_${tabId}`]: false })
+    .then(rv => rv[`quotes_collapsed_in_tab_${tabId}`]);
+}
+
+async function setQuotesCollapsed(tabId, value) {
+  return browser.storage.session
+    .set({ [`quotes_collapsed_in_tab_${tabId}`]: value });
+}
+
+async function toggleQuotes(tabId) {
+  // Toggle the saved state.
+  let quotesCollapsed = !(await getQuotesCollapsed(tabId));
+  await setQuotesCollapsed(tabId, quotesCollapsed);
+  // Force the content script to update.
+  browser.tabs.sendMessage(tabId, { command: "updateQuotes", quotesCollapsed });
+}
+
+// Register an onMessage listener. This listener MUST NEVER be an async function.
+// The listener must ALWAYS return a Promise, or false. Check documentation on MDN
+// for more details.
+browser.runtime.onMessage.addListener((message, sender) => {
+  switch (message?.command) {
+    case "getQuotesCollapsed":
+      return getQuotesCollapsed(sender.tab.id);
+    case "toggleQuotes":
+      return toggleQuotes(sender.tab.id);
+  }
+  // The received message was not for, us. Return false to let other listeners
+  // get a chance to evaluate the message.
+  return false;
+})
+
+// Register a listener for the message display action, to toggle the quotes.
 browser.messageDisplayAction.onClicked.addListener((tab, info) => {
-  browser.toggleQuotesApi.toggleQuotes(tab.windowId);
+  toggleQuotes(tab.id);
 });
 
-browser.messageDisplay.onMessageDisplayed.addListener((tab, message) => {
-  browser.toggleQuotesApi.loadButton(tab.windowId);
+// Register a listener to load the content script into any newly opened message
+browser.messageDisplayScripts.register({
+  js: [{ file: "/toggle_quotes.js" }]
 });
-
-//browser.toggleQuotesApi.loadButton();
---
+++ toggle_quotes.js
@@ -0,0 +1,80 @@
+async function updateQuotes(quotesCollapsed) {
+  let otherQuotes = document.querySelectorAll("blockquote");
+  let firstQuote = document.querySelector("blockquote");
+  if (firstQuote) {
+    if (quotesCollapsed) {
+      for (let e of otherQuotes) {
+        e.setAttribute(
+          "style",
+          `overflow: unset;
+           height: unset;`
+        );
+      };
+      firstQuote.setAttribute(
+        "style",
+        `overflow: unset;
+         height: unset;
+         background-image: url(chrome://messenger/skin/overrides/arrow-down-12.svg);
+         padding-bottom: .3ex;
+         background-repeat: no-repeat;
+         background-position-x: 1px;
+         background-position-y: .4ex;
+         background-size: 1em;`
+      );
+    } else {
+      for (let e of otherQuotes) {
+        e.setAttribute(
+          "style",
+          `overflow: hidden;
+           height: 2ex;
+           padding-bottom: .6ex;`);
+      };
+      firstQuote.setAttribute(
+        "style",
+        `overflow: hidden;
+         height: 2ex;
+         background-image: url(chrome://messenger/skin/overrides/arrow-right-12.svg);
+         padding-bottom: .6ex;
+         background-repeat: no-repeat;
+         background-position-x: 1px;
+         background-position-y: center;
+         background-size: 1em;`
+      );
+    }
+  };
+}
+
+async function init() {
+  let firstQuote = document.querySelector("blockquote");
+  let toggleButton = document.querySelector("button");
+  if (!toggleButton) toggleButton = document.createElement("button");
+  toggleButton.setAttribute(
+    "style",
+    `background-color: transparent;
+         border-color: transparent;
+         margin-inline: -5px 2px;
+         padding-top: 10px;`
+  );
+  toggleButton.addEventListener("click", () => browser.runtime.sendMessage({ command: "toggleQuotes" }));
+
+  if (firstQuote) {
+    firstQuote.insertAdjacentElement("afterbegin", toggleButton);
+    let firstWrap = document.getElementsByTagName("pre").item(1);
+    if (firstWrap) firstWrap.setAttribute("style", "display: inline;"); // fix the first quote if wrapped
+
+    let quotesCollapsed = await browser.runtime.sendMessage({ command: "getQuotesCollapsed" });
+    updateQuotes(quotesCollapsed);
+  };
+
+  // Allow the background to toggle us.
+  browser.runtime.onMessage.addListener(message => {
+    switch (message?.command) {
+      case "updateQuotes": return Promise.resolve(updateQuotes(message.quotesCollapsed))
+    }
+    // The received message was not for, us. Return false to let other listeners
+    // get a chance to evaluate the message.
+    return false;
+  })
+}
+
+init();
